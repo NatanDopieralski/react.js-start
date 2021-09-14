9b83ecbed052fca316175aa5fb18d0d797735fa3
Natan Dopieralski <natandopieralski@gmail.com>
2021-09-14 16:14:35 +0200

    add column.js and fix list.js

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import styles from './List.scss';
-import Hero from '../Hero/Hero'
+import Hero from '../Hero/Hero';
+import PropTypes from 'prop-types';
+import Column from '../Column/Column';
 
 class List extends React.Component {
+  static propTypes = {
+    title: PropTypes.node.isRequired,
+    children: PropTypes.node,
+    img: PropTypes.string,
+  }
+
+  static defaultProps = {
+    children: <p>I can do all the things!!!</p>,
+  }
+
   render() {
     return (
       <section className={styles.component}>
-        <Hero />
+        <Hero titleText={this.props.title} img={this.props.img} />
+        <div className={styles.description}>
+          {this.props.children}
+        </div>
+        <div className={styles.columns}>
+          <Column column={'Animals'} />
+          <Column column={'Plants'}/>
+          <Column column={'Minerals'}/>
+        </div>
       </section>
     )
   }

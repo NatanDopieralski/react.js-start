836c84d68cdb9ee729df5329e81cbd0fcf362324
Natan Dopieralski <natandopieralski@gmail.com>
2021-10-11 22:10:34 +0200

    add Procfile

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import Home from '../Home/HomeContainer';
 import Info from '../Info/Info';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import MainLayout from '../MainLayout/MainLayout';
+import {AnimatedSwitch} from 'react-router-transition';
+import styles from './App.scss';
 
 const App = () => (
-  <MainLayout>
-    <BrowserRouter>
-      <Switch>
+  <BrowserRouter>
+    <MainLayout>
+      <AnimatedSwitch
+        atEnter={{ opacity: 0 }}
+        atLeave={{ opacity: 0 }}
+        atActive={{ opacity: 1 }}
+        className={styles.switchWrapper}
+      >
         <Route exact path='/' component={Home} />
         <Route exact path='/info' component={Info} />
-      </Switch>
-    </BrowserRouter>
-  </MainLayout>
+      </AnimatedSwitch>
+    </MainLayout>
+  </BrowserRouter>
 );
 
 export default App;

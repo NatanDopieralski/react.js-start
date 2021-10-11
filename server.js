4ee3c535bc4d8cc89d93327dc93f2c8f47bb5e85
Natan Dopieralski <natandopieralski@gmail.com>
2021-10-11 22:30:13 +0200

    add Procfile v4

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('dist/db/app.json');
 const middlewares = jsonServer.defaults({
   static: 'dist',
   noCors: true,
@@ -8,6 +7,5 @@ const middlewares = jsonServer.defaults({
 const port = process.env.PORT || 3131;
 
 server.use(middlewares);
-server.use(router);
 
 server.listen(port);
